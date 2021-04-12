91c721c2784d0620e4199e0deae65238b194a72d
harshitaNS <81706072+harshitaNS@users.noreply.github.com>
2021-04-12 21:29:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h3>Hi Harshita!! Welcome to myapp...</h3>
-        <h5>Lets get started</h5>
+        <h5>Lets get</h5>
         <a
           className="App-link"
           href="https://reactjs.org"
